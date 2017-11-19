f1abe658414102a3b41ee2fcb55c31fb2141ddcd
weqopy <weqopy@gmail.com>
2017-11-19 14:40:30 +0800

    update settings.py -> ALLOWED_HOSTS & collectstatic

--- djsite/settings.py
+++ djsite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '33bz833%#4$buud_#boeh$qdyu$ej&yw+9tuwd!!4t$384le(_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djsite.herokuapp.com']
+ALLOWED_HOSTS = ['djsite.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
