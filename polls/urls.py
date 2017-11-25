5a8e68ea110ad3ab5a8c3c36e61fd43267679997
weqopy <weqopy@gmail.com>
2017-11-25 16:45:48 +0800

    add form in detail.html uncompleted

--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
-from . import views as polls_views
+from . import views
 
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$', polls_views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', polls_views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', polls_views.results,
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$',
+        views.ResultsView.as_view(),
         name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', polls_views.vote, name='vote'),
-
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
