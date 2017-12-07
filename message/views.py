60419b13abb498120659529424be6f9df21174a2
weqopy <weqopy@gmail.com>
2017-12-07 20:13:27 +0800

    update message/views.py and form.html

--- message/views.py
+++ message/views.py
@@ -3,24 +3,28 @@ from .models import UserMessage
 
 
 def getform(request):
+    message = None
     # objects.all()获取所有，filter()筛选
     # all_message = UserMessage.objects. all()
     all_message = UserMessage.objects.filter(name='bobb')
-    for message in all_message:
+    if all_message:
+        message = all_message[0]
+    # for message in all_message:
         # 直接输出内容
         # print(message.name)
-        message.delete()
+        # message.delete()
 
     # POST方法
-    # if request.method == 'POST':
-    #     name = request.POST.get('name', '')
-    #     message = request.POST.get('message', '')
-    #     email = request.POST.get('email', '')
-    #     address = request.POST.get('address', '')
-    #     user_message = UserMessage()
-    #     user_message.name = name
-    #     user_message.message = message
-    #     user_message.address = address
-    #     user_message.email = email
-    #     user_message.save()
-    return render(request, 'message/message_form.html')
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        message = request.POST.get('message', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address', '')
+        user_message = UserMessage()
+        user_message.name = name
+        user_message.message = message
+        user_message.address = address
+        user_message.email = email
+        user_message.save()
+    # 在 html 文件中显示变量
+    return render(request, 'message/message_form.html', {'my_message': message})
